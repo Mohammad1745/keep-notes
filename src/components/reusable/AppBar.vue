9baff7c2d39c3c191274aca2a44acd00443ab667
Mohammad Ali <mdali2016.227@gmail.com>
2022-01-28 21:04:00 +0600

    [Add] user name and avatar on app bar

--- src/components/reusable/AppBar.vue
+++ src/components/reusable/AppBar.vue
@@ -3,16 +3,35 @@
 
     <v-app-bar-nav-icon @click="$emit('nav-click')"></v-app-bar-nav-icon>
 
-    <v-app-bar-title>Title</v-app-bar-title>
+    <v-app-bar-title>{{ username }}</v-app-bar-title>
 
     <v-spacer></v-spacer>
+    <v-app-bar-subtitle class="mr-2">
+      <img :src="avatar" alt="" width="35" class="rounded-circle" >
+    </v-app-bar-subtitle>
   </v-app-bar>
 </template>
 
 <script>
 
+import {mapGetters} from "vuex"
+
 export default {
   name: "AppBar",
+  data(){
+    return {
+      username: '',
+      avatar: '',
+    }
+  },
+  mounted(){
+    setTimeout(()=>{
+      let token = sessionStorage.getItem('token')
+      token = JSON.parse(token)
+      this.username = token ? token.user.name : ''
+      this.avatar = token ? token.user.avatar : ''
+    }, 1000)
+  }
 }
 </script>
 
